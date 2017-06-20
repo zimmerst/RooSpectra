c6ab14d748bc998f0c6d3c52117ed9b98b0ce426
zimmer <zimmer@fysik.su.se>
2017-06-20 16:48:58 +0200

    fix

--- src/SlidingWindowFit.C
+++ src/SlidingWindowFit.C
@@ -99,12 +99,15 @@ void SlidingWindowFit::buildModel(){
 
     if (include_proton_bkg){
         RooAbsPdf *proton_bkg = ws->pdf("proton_bkg");
+        RooRealVar nP("nP","number of protons",1.,0,fscale);
+        RooGenericPdf prot_bkg_pdf =("prot_bkg_pdf","proton pdf",RooArgSet(*proton_bkg),RooArgList(nP));
+
         RooAddPdf bmodel("bmodel","bmodel",
-                         RooArgList(pwl_pdf,*proton_bkg),
+                         RooArgList(pwl_pdf,*prot_bkg_pdf),
                          RooArgList(norm));
-        RooAddPdf bmodel1("bmodel1","bmodel1",RooArgList(pwl_exp_pdf,*proton_bkg),RooArgList(norm));
-        RooAddPdf bmodel2("bmodel2","bmodel2",RooArgList(pwl_exp2_pdf,*proton_bkg),RooArgList(norm));
-        RooAddPdf bmodel3("bmodel3","bmodel3",RooArgList(bpl_pdf,*proton_bkg),RooArgList(norm));
+        RooAddPdf bmodel1("bmodel1","bmodel1",RooArgList(pwl_exp_pdf,*prot_bkg_pdf),RooArgList(norm));
+        RooAddPdf bmodel2("bmodel2","bmodel2",RooArgList(pwl_exp2_pdf,*prot_bkg_pdf),RooArgList(norm));
+        RooAddPdf bmodel3("bmodel3","bmodel3",RooArgList(bpl_pdf,*prot_bkg_pdf),RooArgList(norm));
         ws->import(bmodel);
         ws->import(bmodel1);
         ws->import(bmodel2);
